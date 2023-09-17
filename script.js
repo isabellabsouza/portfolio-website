91730513caf3ae9e25e06150428677cd0c7de0f1
Isabella Bicudo de Souza <isabellabicudo@hotmail.com>
2023-09-17 13:14:02 -0300

    navbar
    navbar responsiva -> para desktop e com um ícone de hamburguer para mobile

---
+++ script.js
@@ -0,0 +1,6 @@
+function abreMenu() {
+    const menu = document.querySelector('.mobile-menu-links');
+    const icon = document.querySelector('.hamburguer-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
